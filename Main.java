aff4c9cc24ef2aa05b907def707e361f3169648c
215713G <e215713@ie.u-ryukyu.ac.jp>
2024-12-10 09:51:10 +0900

    step1完了

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        String str = null; // 条件1: String型変数にnullを代入
+        System.out.println(str.length()); // 条件2: str.length()を表示
+    }
+}
